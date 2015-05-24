09125f099f82dc2231a03585ee1ebb09f973f5c4
Maxime Charles <maxfoow@gmail.com>
2015-05-24 12:10:29 +0200

    menu settings

---
+++ Orion.Zeta/Orion.Zeta/Settings/ViewModels/SettingViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+using Orion.Zeta.Controls;
+using Orion.Zeta.ViewModels;
+
+namespace Orion.Zeta.Settings.ViewModels {
+	public class SettingViewModel : BaseViewModel {
+		public ObservableCollection<MenuPanelItem> MenuItems { get; private set; }
+
+		public SettingViewModel() {
+			
+			this.MenuItems = new ObservableCollection<MenuPanelItem> { new MenuPanelItem {
+				Header = "General",
+				Icon = Application.Current.FindResource("appbar_settings") as Canvas
+			},
+			new MenuPanelItem {
+				Header = "Style",
+				Icon = Application.Current.FindResource("appbar_draw_paintbrush") as Canvas
+			}};
+		}
+	}
+}
