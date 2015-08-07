531fa9658354572db96164300786f49f3cfd7cd0
Maxime Charles <maxfoow@gmail.com>
2015-08-07 13:45:15 +0200

    fix executing application

--- Orion.Zeta/Orion.Zeta.Methods.Dev/Shared/Implementations/Execute.cs
+++ Orion.Zeta/Orion.Zeta.Methods.Dev/Shared/Implementations/Execute.cs
@@ -1,17 +1,33 @@
 using System;
 using System.Diagnostics;
+using System.IO;
 
 namespace Orion.Zeta.Methods.Dev.Shared.Implementations {
 	public class Execute : IExecute {
 		public string Program { get; set; }
 
 		public string Parameters { get; set; }
+
+		private ProcessStartInfo _processInfo;
+
 		public void Start() {
-			if (this.HaveParameters()) {
-				Process.Start(this.Program, this.Parameters);
+			if (_processInfo == null)
+				this.GenerateProcessStartInfo();
+			if (this._processInfo == null) {
+				throw new Exception();
 			}
-			else {
-				Process.Start(this.Program);
+			Process.Start(this._processInfo);
+		}
+
+		private void GenerateProcessStartInfo() {
+			if (this.Program == null || !File.Exists(this.Program))
+				throw new Exception();
+			this._processInfo = new ProcessStartInfo {
+				FileName = Path.GetFileName(this.Program),
+				WorkingDirectory = Path.GetDirectoryName(this.Program)
+			};
+			if (this.HaveParameters()) {
+				this._processInfo.Arguments = this.Parameters;
 			}
 		}
 
