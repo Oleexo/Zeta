851f6666adca0f9263355f8c9f79c8084d30770e
Maxime CHARLES <maxfoow@gmail.com>
2015-09-23 13:49:27 +0200

    add debug mod for launcher

--- Orion.Zeta/Orion.Zeta.Launcher/App.xaml.cs
+++ Orion.Zeta/Orion.Zeta.Launcher/App.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System.Configuration;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.IO;
 using System.Windows;
 
@@ -14,7 +13,12 @@ namespace Orion.Zeta.Launcher {
 				Current.Shutdown();
 				return;
 			}
-			var executablePath = Path.Combine(currentPath, ConfigurationManager.AppSettings["applicationExe"]);
+#if !(DEBUG)
+			var executableName = ConfigurationManager.AppSettings["applicationExe"];
+#else
+			var executableName = @"../../../Orion.Zeta/bin/Debug/Zeta.exe";
+#endif
+			var executablePath = Path.Combine(currentPath, executableName);
 			Process.Start(executablePath);
 			Current.Shutdown();
 		}
